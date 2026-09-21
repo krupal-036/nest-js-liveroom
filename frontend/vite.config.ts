7582d6cef87f092f6c9ce84800b339d52a766c49
krupal-036 <krupalfataniya007@gmail.com>
2026-09-21 23:56:59 +0530

    Merge branch 'develop' into main

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -13,13 +13,11 @@ export default defineConfig(({ mode }) => {
     define: {
       'process.env': {} 
     },
-
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
       },
     },
-    
     server: {
       proxy: (mode === 'development' || env.VITE_ENABLE_PROXY === 'true') ? {
         '/api': {
