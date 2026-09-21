3c714062fa0f637d07aec372927414b979d962fb
krupal-036 <krupalfataniya007@gmail.com>
2026-09-21 23:47:38 +0530

    feat: update environment configuration and add Docker commands to package.json

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -11,6 +12,12 @@ export default defineConfig(({ mode }) => {
     
     define: {
       'process.env': {} 
+    },
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
     
     server: {
